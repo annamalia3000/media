29b71f8f1b7654358c1fce8ce27c708d8cc51f6f
annamalia3000 <a.molch@mail.ru>
2024-09-22 23:29:41 +0300

    class text

---
+++ src/js/Text.js
@@ -0,0 +1,51 @@
+import { getGeolocation } from './geolocation/getGeolocation';
+import { formatDate } from './formDate';
+
+export class Text {
+    constructor(containerSelector) {
+        this.media = document.querySelector(containerSelector);
+        this.textInput = document.querySelector('.text-input');
+    }
+
+    initialize() {
+        this.textInput.addEventListener('keydown', (event) => {
+            if (event.key === 'Enter' && this.textInput.value.trim() !== '') {
+                event.preventDefault();  
+                this.addText(); 
+            }
+        });
+    }
+
+    createTextItem(latitude, longitude) {
+        const textItem = document.createElement('div');
+        textItem.classList.add('media-text');
+
+        const textDate = document.createElement('div');
+        textDate.classList.add('date');
+        textDate.textContent = formatDate(new Date()); 
+    
+        const textContent = document.createElement('div');
+        textContent.classList.add('text-content');
+        textContent.textContent = this.textInput.value;
+
+        const userGeo = document.createElement('div');
+        userGeo.classList.add('user-geolocation');
+        userGeo.textContent = `[${latitude}, ${longitude}]`;
+
+        textItem.appendChild(textDate);
+        textItem.appendChild(textContent);
+        textItem.appendChild(userGeo);
+
+        this.textInput.value = '';
+
+        return textItem;
+    }
+
+    addText() {
+        getGeolocation(({ latitude, longitude }) => {
+            const textItem = this.createTextItem(latitude, longitude);  
+            this.media.appendChild(textItem);  
+        });
+    }
+}
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
-import './addNote/addText';
+import  { Text } from './Text';
 import './addAudio/addAudio';
 import './addVideo/addVideo';
+
+const mediaText = new Text('.media-content');
+
+mediaText.initialize();
